0f16ddc3665a09375bfb073ee26cb2384acb47e7
SephyZ <jacobc3@gmail.com>
2015-04-29 17:48:42 +0800

    a little change

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/sephy/github/MyBlog/env"
+set -gx VIRTUAL_ENV "/Users/Sephy/code/myProfile/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
