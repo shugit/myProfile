90312800ae45806d3f80f9148d22b2068f8de0b8
SephyZ <shuwenz@outlook.com>
2015-05-05 01:11:09 -0700

    modify

--- app/forms.py
+++ app/forms.py
@@ -20,13 +20,6 @@ class EditProjectForm(Form):
     start_time = DateField('start time', format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
     end_time = DateField('end time', format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
     facility = SelectField('facility', coerce=int)
-    def __init__(self, select):
-        print '============'
-        print 'initing'
-        print select
-        super(EditProjectForm,self).__init__()
-        self.set_choices()
-
 
     def set_choices(self):
         facility_list = Facility.query.order_by(Facility.start_time).all()
@@ -51,3 +44,33 @@ class EditFacilityForm(Form):
             choices.append([project.id,project.name])
         self.projects.choices = choices
 
+
+class AddFacilityForm(Form):
+    name = StringField('name',validators=[DataRequired(),Length(min=1,max=140)])
+    start_time = DateField('start time', validators=[], format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
+    end_time = DateField('end time',validators=[], format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
+    position = StringField('position')
+    location = StringField('location')
+    type = SelectField('type', choices=[(1,'School'),(2,'Company')],coerce=int)
+    projects = SelectMultipleField('projects')
+
+    def set_projects_choices(self):
+        choices = []
+        for project in Project.query.all():
+            choices.append([project.id,project.name])
+        self.projects.choices = choices
+
+
+class AddProjectForm(Form):
+    name = StringField('name',validators=[DataRequired()])
+    description = TextAreaField('description', validators=[DataRequired()])
+    start_time = DateField('start time', format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
+    end_time = DateField('end time', format=app.config.get('DATE_FORMAT'),default=datetime.datetime.now())
+    facility = SelectField('facility', coerce=int)
+
+    def set_choices(self):
+        facility_list = Facility.query.order_by(Facility.start_time).all()
+        choices = []
+        for facility in facility_list:
+            choices.append([facility.id, facility.name])
+        self.facility.choices = choices
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from app import app, db
 from models import *
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from .forms import LoginForm, EditProjectForm, EditFacilityForm
+from .forms import LoginForm, EditProjectForm, EditFacilityForm, AddFacilityForm, AddProjectForm
 import helper
 import pprint
 
@@ -27,8 +27,8 @@ def index():
                            skills=the_skills,
                            publications=the_publications
                            )
-
-
+'''Project'''
+'''Edit'''
 @app.route('/project/<int:project_id>')
 def project(project_id):
     project = Project.query.get(project_id)
@@ -44,20 +44,25 @@ def project(project_id):
 @app.route('/project/<int:project_id>/edit')
 def edit_project_form(project_id):
     the_project = Project.query.get(project_id)
-    form = EditProjectForm('1')
+    form = EditProjectForm()
+    form.set_choices()
     form.name.data = the_project.name
     form.description.data = the_project.description
+    print form.facility.default
     if the_project.start_time is not None:
         form.start_time.data = the_project.start_time
     if the_project.end_time is not None:
         form.end_time.data = the_project.end_time
-    return render_template('edit/project.html', form=form, projects=helper.getProjects())
+    return render_template('edit/project.html',
+                           form=form,
+                           projects=helper.getProjects())
 
 
 @app.route('/project/<int:project_id>/edit', methods=['GET', 'POST'])
 def edit_project(project_id):
     form = EditProjectForm(request.form)
     project = Project.query.get(project_id)
+    form.set_choices()
     if form.validate_on_submit():
         project.name = form.name.data
         project.description = form.description.data
@@ -67,16 +72,49 @@ def edit_project(project_id):
         db.session.add(project)
         db.session.commit()
         flash('Your change is saved to project %s' % project.name)
-        return redirect(url_for('project', project_id=project.id))
+        return redirect(url_for('project',
+                                project_id=project.id))
     else:
         flash('Your change is not saved to project %s' % project.name)
-    return render_template('edit/project.html', form=form, projects=helper.getProjects())
+    return render_template('edit/project.html',
+                           form=form,
+                           projects=helper.getProjects())
+
+'''Add Project'''
+@app.route('/project/add')
+def add_project_form():
+    form = AddProjectForm()
+    form.set_choices()
+    return render_template('add/project.html',
+                           form=form,
+                           projects=helper.getProjects())
 
 
-'''
-facility
-'''
+@app.route('/project/add', methods=['GET', 'POST'])
+def add_project():
+    form = AddProjectForm(request.form)
+    form.set_choices()
+    if form.validate_on_submit():
+        project = Project()
+        project.name = form.name.data
+        project.description = form.description.data
+        project.start_time = form.start_time.data
+        project.end_time = form.end_time.data
+        project.facility = form.facility.data
+        db.session.add(project)
+        db.session.commit()
+        flash('Your add is saved to project %s' % project.name)
+        return redirect(url_for('project',
+                                project_id=project.id))
+    else:
+        flash('Your add is not saved to project %s' % project.name)
+    return render_template('edit/project.html',
+                           form=form,
+                           projects=helper.getProjects())
 
+
+'''Facility'''
+'''Edit'''
 
 @app.route('/facility/<int:facility_id>/edit')
 def edit_facility_form(facility_id):
@@ -91,7 +129,9 @@ def edit_facility_form(facility_id):
         form.end_time.data = the_facility.end_time
     form.set_projects_choices()
     form.type.data = '8'
-    return render_template('edit/facility.html', form=form, projects=helper.getProjects())
+    return render_template('edit/facility.html',
+                           form=form,
+                           projects=helper.getProjects())
 
 
 @app.route('/facility/<int:facility_id>/edit', methods=['GET', 'POST'])
@@ -111,7 +151,41 @@ def edit_facility(facility_id):
         return redirect(url_for('index'))
     else:
         flash('Your change is not saved to project %s' % facility.name)
-    return render_template('edit/facility.html', form=form, projects=helper.getProjects())
+    return render_template('edit/facility.html',
+                           form=form,
+                           projects=helper.getProjects())
+
+
+'''Add Facility'''
+@app.route('/facility/add')
+def add_facility_form():
+    form = AddFacilityForm()
+    form.set_projects_choices()
+    return render_template('add/facility.html',
+                           form=form,
+                           projects=helper.getProjects())
+
+@app.route('/facility/add', methods=['GET', 'POST'] )
+def add_facility():
+    form = AddFacilityForm()
+    form.set_projects_choices()
+    facility = Facility()
+    if form.validate_on_submit():
+        facility.name = form.name.data
+        facility.start_time = form.start_time.data
+        facility.end_time = form.end_time.data
+        facility.location = form.location.data
+        facility.position = form.position.data
+        facility.type = 'school' if form.type.data == 1 else 'company'
+        db.session.add(facility)
+        db.session.commit()
+        flash('Your change is saved to project %s' % facility.name)
+        return redirect(url_for('index'))
+    else:
+        flash('Your change is not saved to project %s' % facility.name)
+    return render_template('add/facility.html',
+                           form=form,
+                           projects=helper.getProjects())
 
 
 """
